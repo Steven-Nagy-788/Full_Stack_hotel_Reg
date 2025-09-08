17b8154438b2498d76d088378a88f150feaef1ac
Steven Nagy <stevennagy788@gmail.com>
2025-09-08 14:08:44 +0300

    updated room type gets

--- BackEnd/Hotel_Reserv/Hotel_Reserv/Controllers/RoomTypesController.cs
+++ BackEnd/Hotel_Reserv/Hotel_Reserv/Controllers/RoomTypesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,102 +7,34 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Hotel_Reserv.Data;
 using Hotel_Reserv.Models;
+using Hotel_Reserv.Services;
 
-namespace Hotel_Reserv.Controllers
+namespace Hotel_Reserv.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoomTypesController(IRoomTypeService roomTypeService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RoomTypesController : ControllerBase
+    [HttpGet]
+    public async ValueTask<IResult> GetRoomTypes() => await roomTypeService.GetRoomTypes();
+    [HttpGet("{id}")]
+    public async ValueTask<IResult> GetRoomType(int id) => await roomTypeService.GetRoomType(id);
+
+    // PUT: api/RoomTypes/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async ValueTask<IResult> PutRoomType(int id, RoomType roomType)
     {
-        private readonly ApplicationDbContext _context;
+        return await roomTypeService.UpdateRoomType(id, roomType);
+    }
 
-        public RoomTypesController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    [HttpPost]
+    public async ValueTask<IResult> PostRoomType(RoomType roomType) => await roomTypeService.CreateRoomType(roomType);
 
-        // GET: api/RoomTypes
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<RoomType>>> GetRoomTypes()
-        {
-            return await _context.RoomTypes.ToListAsync();
-        }
-
-        // GET: api/RoomTypes/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<RoomType>> GetRoomType(int id)
-        {
-            var roomType = await _context.RoomTypes.FindAsync(id);
-
-            if (roomType == null)
-            {
-                return NotFound();
-            }
-
-            return roomType;
-        }
-
-        // PUT: api/RoomTypes/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutRoomType(int id, RoomType roomType)
-        {
-            if (id != roomType.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(roomType).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RoomTypeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/RoomTypes
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<RoomType>> PostRoomType(RoomType roomType)
-        {
-            _context.RoomTypes.Add(roomType);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetRoomType", new { id = roomType.Id }, roomType);
-        }
-
-        // DELETE: api/RoomTypes/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteRoomType(int id)
-        {
-            var roomType = await _context.RoomTypes.FindAsync(id);
-            if (roomType == null)
-            {
-                return NotFound();
-            }
-
-            _context.RoomTypes.Remove(roomType);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool RoomTypeExists(int id)
-        {
-            return _context.RoomTypes.Any(e => e.Id == id);
-        }
+    // DELETE: api/RoomTypes/5
+    [HttpDelete("{id}")]
+    public async ValueTask<IResult> DeleteRoomType(int id)
+    {
+        return await roomTypeService.DeleteRoomType(id);
     }
 }
