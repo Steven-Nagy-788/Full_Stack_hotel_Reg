e19511ae7c6f60cf8aa295ec22ea7c6e2d120f0d
Paula Moukhtar <paulamoukhtar@gmail.com>
2025-09-09 08:47:06 +0300

    Update IRoomTypeService.cs

--- BackEnd/Hotel_Reserv/Hotel_Reserv/Services/IRoomTypeService.cs
+++ BackEnd/Hotel_Reserv/Hotel_Reserv/Services/IRoomTypeService.cs
@@ -1,17 +1,1 @@
-using Hotel_Reserv.Models;
-using Hotel_Reserv.Models.Dtos;
-using Hotel_Reserv.Models.Dtos.RoomtypeDto;
-namespace Hotel_Reserv.Services;
 
-public interface IRoomTypeService
-{
-    ValueTask<IResult> GetRoomTypes();
-    ValueTask<IResult> GetRoomType(int id);
-<<<<<<< Updated upstream
-    ValueTask<IResult> CreateRoomType(RoomType roomType);
-=======
-    ValueTask<IResult> CreateRoomType(RoomTypeDto roomType);
->>>>>>> Stashed changes
-    ValueTask<IResult> UpdateRoomType(int id, RoomType roomType);
-    ValueTask<IResult> DeleteRoomType(int id);
-}
